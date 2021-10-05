01ef928bfd7100ba9bc26be3aaa7c0d7eb306873
Jagannath Pidaparthy <jagan241296@gmail.com>
2021-10-05 11:39:50 +0530

    initial commit

---
+++ src/main/java/app/portal/FreshersPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package app.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshersPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshersPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/portal/FreshersPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshersPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
